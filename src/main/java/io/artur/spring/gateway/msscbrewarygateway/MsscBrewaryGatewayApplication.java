6464428796bcc346b5d510a00aaaf0ffd8af0029
Artur Przychodzen <artur2p@gmail.com>
2021-11-05 09:54:39 +0100

    inintial

---
+++ src/main/java/io/artur/spring/gateway/msscbrewarygateway/MsscBrewaryGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.gateway.msscbrewarygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewaryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewaryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/gateway/msscbrewarygateway/MsscBrewaryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.gateway.msscbrewarygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewaryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
