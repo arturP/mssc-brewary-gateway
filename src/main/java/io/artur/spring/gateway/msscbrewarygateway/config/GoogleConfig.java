75e21adbb7af7c229920daa251a48e42b9ecac6a
Artur Przychodzen <artur2p@gmail.com>
2021-11-05 11:38:38 +0100

    redirection to googole.com added with configuration

---
+++ src/main/java/io/artur/spring/gateway/msscbrewarygateway/config/GoogleConfig.java
@@ -0,0 +1,25 @@
+package io.artur.spring.gateway.msscbrewarygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ */
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleSearchConfig(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(r -> r.path("/googlesearch")
+                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com"))
+                .build();
+    }
+}
